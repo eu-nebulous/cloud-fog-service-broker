21e407ff3a39c8ba52c9848fe785645ac3f693b6
gregkoron <gregkoron@gmail.com>
2026-01-29 22:52:37 +0200

    HomePage HTML changes

--- cfsb-frontend/src/components/HomePage.vue
+++ cfsb-frontend/src/components/HomePage.vue
@@ -93,13 +93,11 @@ export default {
       <div class="col col-12 col-lg-3">
         <div class="card">
           <div class="card-header">
-            <h2>Select Policy</h2>
+            <h2 class="fw-normal">Policy</h2>
           </div>
           <div class="card-body text-start">
             <p class="description">
-              Policy used for the criteria in the evaluation
-              <!-- Put this in bubble: Rank at highest positions the nodes with lower RAM and # of Cores (Minimal policy).The opposite holds for Maximal policy.
-              -->
+              Select the policy used for the criteria in the evaluation
             </p>
             <form>
               <div class="form-check">
@@ -117,16 +115,16 @@ export default {
       <div class="col col-12 col-lg-3">
         <div class="card">
           <div class="card-header">
-            <h2>Use All or Specific Nodes</h2>
+            <h2 class="fw-normal">Node Usage</h2>
           </div>
           <div class="card-body text-start">
             <p class="description">
-              Employ all available or application-specific nodes
+              Employ all the available nodes or the application-specific nodes only
             </p>
             <form>
               <div class="form-check">
               <input v-model="nodesModeChoice" @change="onChoiceChange" class="form-check-input" type="radio" name="nodes_mode_choice" id="nodes_choice_all" value="0">
-              <label class="form-check-label" for="nodes_choice_all">All Available Nodes</label>
+              <label class="form-check-label" for="nodes_choice_all">All Nodes</label>
               </div>
               <div class="form-check">
                 <input v-model="nodesModeChoice" @change="onChoiceChange" class="form-check-input" type="radio" name="nodes_mode_choice" id="nodes_choice_own" value="1" checked>
@@ -139,24 +137,24 @@ export default {
       <div class="col col-12 col-lg-3">
         <div class="card">
           <div class="card-header">
-            <h2>Select Location</h2>
+            <h2 class="fw-normal">Location</h2>
           </div>
           <div class="card-body text-start">
             <p class="description">
-              Type the name of the location (country and/or city), for nodes filtering.
+              Provide the location (country and/or city) for nodes filtering.
             </p>
             <form>
               <input v-model="nodesLocation.country" type="text" class="form-control" id="nodes_location_country_choice" name="nodes_location_country_choice" placeholder="e.g. Norway">
               <p></p>
               <input v-model="nodesLocation.city" type="text" class="form-control" id="nodes_location_city_choice" name="nodes_location_city_choice" placeholder="e.g. Bergen">
-              <div class="form-text">Leave blank to fetch nodes without location filtering</div>
+              <div class="form-text fw-bold">Leave blank to fetch nodes without location filtering</div>
             </form>
           </div>
         </div>
       </div>
       <div class="col col-12 col-lg-3">
-        <h3 class="display-6">Try now</h3>
-        <router-link to="/criteria-selection" class="btn button-primary btn-lg" @click.native="get_project_choices">Criteria Selection</router-link>
+        <h3 class="display-6">Select Criteria</h3>
+        <router-link to="/criteria-selection" class="btn button-primary btn-lg" @click.native="get_project_choices">Apply and Proceed</router-link>
       </div>
     </div>
     <div class="spacer-sm"></div>
